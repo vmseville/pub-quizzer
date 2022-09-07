c62de639f87a5abd1f19e5efaaf90146c452669d
vmseville <vmseville@gmail.com>
2022-09-07 21:13:39 +0800

    add primeng and initial changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'pub-quizzer';
+  title = 'Pub Quiz Night';
 }
