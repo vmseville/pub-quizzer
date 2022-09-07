976332090d9b2ec8eb8cb6dc81e8991fa05c740d
vmseville <vmseville@gmail.com>
2022-09-07 22:00:33 +0800

    add pub-intro component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PubIntroComponent } from './pub-intro/pub-intro.component';
+import { ButtonModule } from 'primeng/button';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, PubIntroComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, ButtonModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/pub-intro/pub-intro.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'qzr-pub-intro',
+  templateUrl: './pub-intro.component.html',
+  styleUrls: ['./pub-intro.component.scss']
+})
+export class PubIntroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
