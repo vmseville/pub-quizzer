c022b6e378390f36732610da5b6b602f3a7bd555
vmseville <vmseville@gmail.com>
2022-09-11 13:45:24 +0800

    add initial reactive form for game setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,6 +19,7 @@ import { FieldsetModule } from 'primeng/fieldset';
     BrowserAnimationsModule,
     AppRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
     ButtonModule,
     InputNumberModule,
     FieldsetModule,
--- src/app/game-setup/game-setup.component.ts
+++ src/app/game-setup/game-setup.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'qzr-game-setup',
   templateUrl: './game-setup.component.html',
-  styleUrls: ['./game-setup.component.scss']
+  styleUrls: ['./game-setup.component.scss'],
 })
 export class GameSetupComponent implements OnInit {
+  setupForm = this.formBuilder.group({
+    numberOfQuestions: this.formBuilder.group({
+      easy: [1, [Validators.min(1), Validators.max(10)]],
+      medium: [1, [Validators.min(1), Validators.max(10)]],
+      hard: [1, [Validators.min(1), Validators.max(10)]],
+    }),
+    categories: [[], [Validators.minLength(1)]],
+    questionTypes: [[], [Validators.minLength(1)]],
+  });
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
+    this.setupForm.valueChanges.subscribe((groupValue) => {
+      console.log(`VALUE: ${JSON.stringify(groupValue)}`);
+    });
   }
-
 }
